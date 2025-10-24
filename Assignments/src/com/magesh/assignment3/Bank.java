691aaeafe6d84cdf606d4741a492a721493047f9
HP <HP@DESKTOP-OE5ODK7>
2025-10-24 23:11:30 +0530

    assignment 345 completed

---
+++ Assignments/src/com/magesh/assignment3/Bank.java
@@ -0,0 +1,14 @@
+package com.magesh.assignment3;
+
+public class Bank {
+	 private static int totalAccounts = 0;
+
+	    public Bank() {
+	        totalAccounts++;
+	    }
+
+	    public static int getTotalAccounts() {
+	        return totalAccounts;
+	    }
+
+}
