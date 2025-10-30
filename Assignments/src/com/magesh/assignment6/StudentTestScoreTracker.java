4bdbf89a5f91b90645f1d3f1aac883e27dd99752
HP <HP@DESKTOP-OE5ODK7>
2025-10-30 21:49:05 +0530

    day 6 assignment completed

--- Assignments/src/com/magesh/assignment6/StudentTestScoreTracker.java
+++ Assignments/src/com/magesh/assignment6/StudentTestScoreTracker.java
@@ -1,5 +1,20 @@
 package com.magesh.assignment6;
 
-public class StudentTestScoreTracker {
+import java.util.*;
 
+public class StudentTestScoreTracker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of test scores: ");
+        int n = sc.nextInt();
+
+        StudentScores student = new StudentScores(n);
+
+        student.inputScores(sc);
+        student.convertToArrayList();
+        student.displayScores();
+
+        sc.close();
+    }
 }
